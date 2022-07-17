326effd06d44a8f862d226d87f237877787104a1
Slava Khripushin <slava.cpa@yandex.ru>
2022-07-17 17:41:20 +0300

    33.js

---
+++ js-basics/33.js
@@ -0,0 +1,12 @@
+const getNumberExplanation = (number) => {
+  switch (number) {
+    case 666:
+      return 'devil number';
+    case 7:
+      return 'prime number';
+    case 42:
+      return 'answer for everything';
+    default:
+      return null;
+  }
+};
